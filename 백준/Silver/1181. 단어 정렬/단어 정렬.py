56621272e21df44d9a9524b22c180a0c4bfbf837
horororok <135201712+horororok@users.noreply.github.com>
2023-06-14 15:11:49 +0900

    [Silver V] Title: 단어 정렬, Time: 804 ms, Memory: 35868 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+Lst =[]                 # 입력값을 리스트로 만들기 위해 빈 리스트 준비
+for i in range(n):      
+    Lst.append(input()) # 입력값을 리스트로 만들어 준다
+
+setLst = set(Lst)       # 중복값을 제거 하기 위해 set로 만들어준다
+Lst = list(setLst)      # set를 다시 리스트로 만들어 준다
+Lst.sort()              # sort를 이용해서 알파벳 순 정렬
+Lst.sort(key = len)     # sort를 이용해서 길이 순 정렬
+
+for i in Lst:           # 리스트 형으로 만들어준 리스트를 다시 풀어 준다.
+    print(i)
+
+# sort는 문자도 알파벳 순으로 정렬을 해준다!!
