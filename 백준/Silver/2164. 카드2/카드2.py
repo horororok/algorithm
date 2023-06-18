3df15ba1ec2132c309c0a97c209baa18cef9d389
horororok <135201712+horororok@users.noreply.github.com>
2023-06-18 17:45:47 +0900

    [Silver IV] Title: 카드2, Time: 216 ms, Memory: 50920 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+
+deq = deque()                   #덱 : 큐 선언
+for i in range(n):
+    deq.append(i + 1)
+
+while len(deq) > 1:
+    deq.popleft()               #첫 번째 값을 반환하고 제거
+    deq.append(deq.popleft())   #제일 끝에 반환된 첫 번째 값 추가
+
+print(deq.pop())                #마지막에 남은 값 반환
+
+#큐 의 형식 이해
