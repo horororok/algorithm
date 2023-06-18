3a576c683d394b8195518e0f018f5ed3df397705
horororok <135201712+horororok@users.noreply.github.com>
2023-06-18 17:46:46 +0900

    [Silver IV] Title: 카드2, Time: 212 ms, Memory: 50920 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -11,6 +11,6 @@ while len(deq) > 1:
     deq.popleft()               #첫 번째 값을 반환하고 제거
     deq.append(deq.popleft())   #제일 끝에 반환된 첫 번째 값 추가
 
-print(deq.pop())                #마지막에 남은 값 반환
+print(deq.popleft())                #마지막에 남은 값 반환
 
 #큐 의 형식 이해
