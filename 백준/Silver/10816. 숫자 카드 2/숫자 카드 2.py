e69958ef927307632a4c585eea1c4d8b02ccbe63
horororok <135201712+horororok@users.noreply.github.com>
2023-06-19 20:36:07 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 980 ms, Memory: 114500 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -13,10 +13,12 @@ for i in numCard:                   #갖고 있는 숫자카드의 리스트
     else:                           #없으면
         cnt[i] = 1                  #1
 
+result = []
 for i in numArr:                    #구해야할 숫자의 리스트
     if i in cnt:                    #dict 내에 있으면
-        print(cnt[i], end = " ")    #cnt의 인덱스값(개수)과 공백 출력
+        result.append(cnt[i])    #cnt의 인덱스값(개수)과 공백 출력
     else:                           #없으면
-        print(0, end =" ")          #0과 공백 출력
+        result.append(0)          #0과 공백 출력
+print(*result)
 
 #dict에 개수를 저장해놓고, key에 대한 것이 존재하면 개수를 반환하고 그 외에는 0을 반환하는 방법으로 결과 도출
