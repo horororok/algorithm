99d1e34568d5f4674e80bd4bba65eb13fe740422
horororok <135201712+horororok@users.noreply.github.com>
2023-07-02 21:20:49 +0900

    [Silver IV] Title: 제로, Time: 84 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,14 +1,17 @@
-
+import sys
+input = sys.stdin.readline
 
 k = int(input())
 
 
 List = []
 for i in range(k):
-    num = int(input())
+    num = int(input().strip())
 
     if num == 0:
         List.pop()
     else:
         List.append(num)
 print(sum(List))
+
+#for문 내에서 입력을 받아주는 것 까먹지 말자
