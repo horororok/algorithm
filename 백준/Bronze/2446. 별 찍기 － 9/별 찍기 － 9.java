5f92f042db120205c2ee32e1938cb13b9767232e
horororok <135201712+horororok@users.noreply.github.com>
2023-07-30 19:26:58 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 456 ms, Memory: 16324 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = n; i>0;i--) {
+			for(int j = n+i-1; j>0;j--) {
+				if (j<2*i) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		for(int i = 2; i<n+1;i++) {
+			for(int j=1;j<n+i;j++) {
+				if(j>n-i) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
