527e51b00fd4216976afd9958223b7260013b857
horororok <135201712+horororok@users.noreply.github.com>
2023-08-17 00:26:40 +0900

    [D3] Title: 의석이의 세로로 말해요, Time: 163 ms, Memory: 24,196 KB -BaekjoonHub

--- SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.java
+++ SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) throws FileNotFoundException {
-        // TODO Auto-generated method stub
+        
         //System.setIn(new FileInputStream("src/Day0816_5356/input_5356.txt"));
 
         Scanner sc = new Scanner(System.in);
@@ -17,19 +17,22 @@ public class Solution {
 
         // 각 테스트 케이스에 대해 반복
         for (int tc = 1; tc <= T; tc++) {
-            // 각 테스트 케이스는 다섯 줄로 이루어져 있음
-            int TCase = 5;
-            // 이차원 문자 배열 선언
-            char[][] arr = new char[TCase][15]; // 최대 열 개수를 15로 설정
+        	// 각 테스트 케이스는 다섯 줄로 이루어져 있음
+            int TCase = 5; 
+         // 최대 열 개수를 15로 설정
+            char[][] arr = new char[TCase][15]; 
+
+         // 열의 최대 개수 초기화
+            int maxCol = 0; 
 
             // 각 줄에 대해 반복
-            int maxCol = 0;
             for (int i = 0; i < TCase; i++) {
-                // 문자열 입력
+            	 // 문자열 입력
                 String input = sc.nextLine();
                 // 최대 길이 15로 제한
                 int length = Math.min(15, input.length());
-                maxCol = Math.max(maxCol, length); // 열의 최대 개수 갱신
+             // 열의 최대 개수 갱신
+                maxCol = Math.max(maxCol, length); 
 
                 // 문자열을 문자 하나하나로 쪼개서 배열에 저장
                 for (int j = 0; j < length; j++) {
@@ -37,15 +40,20 @@ public class Solution {
                 }
             }
 
-            // 배열 출력
+            // 출력 시작
             System.out.print("#" + tc + " ");
+
+            // 열 기준으로 출력
             for (int i = 0; i < maxCol; i++) {
                 for (int j = 0; j < TCase; j++) {
-                    if (arr[j][i] != '\u0000') { // 널 문자가 아닐 때 출력
+                    // 해당 열에 값이 있을 때만 출력
+                    if (arr[j][i] != '\u0000') {
                         System.out.print(arr[j][i]);
                     }
                 }
             }
+
+            // 줄 바꿈
             System.out.println();
         }
     }
