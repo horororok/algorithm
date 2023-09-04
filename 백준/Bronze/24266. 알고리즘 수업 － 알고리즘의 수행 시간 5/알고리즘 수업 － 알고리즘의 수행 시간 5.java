6729fe1136483ceac6d4f72a4d267de0ab851f2d
Kim Tae Hun <135201712+horororok@users.noreply.github.com>
2023-09-04 12:25:26 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 212 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		
+		System.out.println(n*n*n);
+		System.out.println(3);
+	}
+
+}
