1a67fce4011c44dccc657cd8856220a0de50b7dc
Kim Tae Hun <135201712+horororok@users.noreply.github.com>
2023-09-10 09:25:40 +0900

    [Silver III] Title: 문자열 집합, Time: 1092 ms, Memory: 68324 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,41 +1,39 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-
-	public static int N; // 기존 문자열의 개수
-	public static int M; // 입력되는 문자열의 개수
-	public static String[] arr1;
-	public static String[] arr2;
-
-	public static int cnt;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
-		N = sc.nextInt();
-		M = sc.nextInt();
+		int N = sc.nextInt();
+		int M = sc.nextInt();
 
-		arr1 = new String[N];
-		for (int i = 0; i < N; i++) {
-			arr1[i] = sc.next();
-		}
-		arr2 = new String[M];
-		for (int i = 0; i < M; i++) {
-			arr2[i] = sc.next();
-		}
-
+//		HashMap<String, Integer> map = new HashMap<>();
+//		for (int i = 0; i < N; i++) {
+//			map.put(sc.next(), 0);
+//		}
+//		int cnt = 0;
+//		for (int i = 0; i < M; i++) {
+//			if (map.containsKey(sc.next())) {
+//				cnt++;
+//			}
+//		}
 		
-		//완전탐색
-		cnt = 0;
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++) {
-				if (arr2[i].equals(arr1[j])) {
-					cnt++;
-				}
+		HashSet<String> set = new HashSet<>();
+		
+		for(int i = 0; i<N; i++) {
+			set.add(sc.next());
+		}
+		
+		int cnt = 0;
+		for(int i = 0; i<M; i++) {
+			if(set.contains(sc.next())) {
+				cnt++;
 			}
 		}
-
+		
 		System.out.println(cnt);
 
 	}
