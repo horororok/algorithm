1de2eb7ceb03d946071e975e06f1d2fa08d6983e
Kim Tae Hun <135201712+horororok@users.noreply.github.com>
2023-10-12 15:27:15 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 236 ms, Memory: 18512 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,68 +1,91 @@
 import java.util.Scanner;
 
 public class Main {
-    static int N, M, D, result;
-    static int[] archer;
-    static int[][] arr, copyarr;
-    static Scanner scanner = new Scanner(System.in);
+	static int N, M, D, result;
+	static int[] archer;
+	static int[][] arr, copyarr;
 
-    public static void main(String[] args) {
-        N = scanner.nextInt();
-        M = scanner.nextInt();
-        D = scanner.nextInt();
-        arr = new int[N][M];
-        copyarr = new int[N][M];
-        archer = new int[3];
+	public static void main(String[] args) {
+		// 입력
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		D = sc.nextInt();
+		arr = new int[N][M];
+		copyarr = new int[N][M];
+		archer = new int[3];
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = scanner.nextInt();
-            }
-        }
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
 
-        combination(0, 0);
-        System.out.println(result);
-    }
+		// 궁수 위치의 최적 조합 찾기
+		combination(0, 0);
 
-    public static void combination(int start, int idx) {
-        if (idx == 3) {
-            result = Math.max(result, game());
-            return;
-        }
+		// 결과 출력
+		System.out.println(result);
+	}
 
-        for (int i = start; i < M; i++) {
-            archer[idx] = i;
-            combination(i + 1, idx + 1);
-        }
-    }
+	// 궁수 위치의 모든 조합을 찾는 재귀 함수
+	public static void combination(int start, int idx) {
+		if (idx == 3) {
+			// 세 궁수 위치가 선택되면 이 조합에 대한 결과 계산
+			result = Math.max(result, game());
+			return;
+		}
 
-    public static int game() {
-        int total = 0;
-        copyarr = new int[N][M];
+		for (int i = start; i < M; i++) {
+			// 다양한 궁수 위치의 조합을 시도
+			archer[idx] = i;
+			combination(i + 1, idx + 1);
+		}
+	}
 
-        for (int line = N; line > 0; line--) {
-            for (int pick : archer) {
-                for (int dist = 1; dist <= D; dist++) {
-                    int kill = attack(copyarr, dist, line, pick);
-                    if (kill < 0) continue;
-                    total += kill;
-                    break;
-                }
-            }
-        }
-        return total;
-    }
+	// 특정 궁수 위치 조합에 대한 게임 시뮬레이션
+	public static int game() {
+		int total = 0;
+		// 원본 보드를 수정하지 않고 공격을 시뮬레이션하기 위해 게임 보드의 복사본 생성
+		copyarr = new int[N][M];
 
-    public static int attack(int[][] copyarr, int dist, int line, int pick) {
-        for (int m = pick - dist; m <= pick + dist; m++) {
-            int n = line - (dist - Math.abs(pick - m));
+		// 각 적 라인에 대해 반복
+		for (int line = N; line > 0; line--) {
+			// 선택된 각 궁수에 대해 반복
+			for (int pick : archer) {
+				// 궁수가 공격할 수 있는 가능한 거리에 대해 반복
+				for (int dist = 1; dist <= D; dist++) {
+					// 공격을 수행하고 적을 처치한 수를 얻음
+					int kill = attack(copyarr, dist, line, pick);
+					if (kill < 0)
+						continue; // 해당 방향에 유효한 대상이 없으면 계속 진행
+					total += kill;
+					break; // 궁수는 턴당 하나의 적만 공격 가능
+				}
+			}
+		}
+		return total; // 이 시뮬레이션에서 처치된 전체 적의 수 반환
+	}
 
-            if (n < 0 || m < 0 || n >= line || m >= M || arr[n][m] == 0) continue;
-            if (copyarr[n][m] == 0) {
-                copyarr[n][m] = line;
-                return 1;
-            } else if (copyarr[n][m] == line) return 0;
-        }
-        return -1;
-    }
+	// 특정 거리와 방향에 대한 공격을 시뮬레이션
+	public static int attack(int[][] copyarr, int dist, int line, int pick) {
+		// 궁수가 공격할 수 있는 가능한 위치에 대해 반복
+		for (int m = pick - dist; m <= pick + dist; m++) {
+			int n = line - (dist - Math.abs(pick - m));
+
+			// 대상 위치가 유효하고 해당 위치에 적이 있는지 확인
+			if (n < 0 || m < 0 || n >= line || m >= M || arr[n][m] == 0)
+				continue;
+
+			// 대상 위치가 이미 이 턴에 공격당했는지 확인
+			if (copyarr[n][m] == 0) {
+				// 복사본에서 해당 위치의 적을 처치했다고 표시하여 중복 계산 방지
+				copyarr[n][m] = line;
+				return 1; // 적이 처치되었음을 나타내는 1 반환
+			} else if (copyarr[n][m] == line) {
+				return 0; // 해당 대상이 이미 이 턴에 공격당했음을 나타내는 0 반환
+			}
+		}
+		return -1; // 해당 방향에 유효한 대상이 없음을 나타내는 -1 반환
+	}
 }
