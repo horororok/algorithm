8e6e29023a141d6966da0f60a7601ef013e5d76f
Kim Tae Hun <135201712+horororok@users.noreply.github.com>
2024-11-16 09:56:27 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,7 @@
 function solution(nums) {
     let len = nums.length/2;
-    let ans=[];
-    for(let val of nums){
-        if(ans.includes(val)){
-            continue;
-        }else{
-            ans.push(val)
-        }
-    }
-    return ans.length>len?len:ans.length
+    let max = new Set(nums);
+    return len>max.size?max.size:len
+    
     
 }
